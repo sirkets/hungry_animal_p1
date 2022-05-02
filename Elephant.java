e2ccf8cdcd6350a91fb8bbafc55f2dd0c619cc8d
Stewart Chan <35233916+sirkets@users.noreply.github.com>
2022-05-02 08:42:55 -0400

    added elephant

---
+++ Elephant.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Elephant extends Actor
+{
+    public void act()
+    {
+        // Add your action code here.
+        move(1);
+        turn(1);
+    }
+}
