575806e6a393b0a85d609c39768546a3caa89fb2
Stewart Chan <stew.chan@gmail.com>
2022-05-18 08:42:39 -0400

    added falling apple and movement

--- Apple.java
+++ Apple.java
@@ -15,5 +15,8 @@ public class Apple extends Actor
     public void act()
     {
         // Add your action code here.
+        int x = getX();
+        int y = getY();
+        setLocation(x, y+2);
     }
 }
--- Elephant.java
+++ Elephant.java
@@ -5,7 +5,11 @@ public class Elephant extends Actor
     public void act()
     {
         // Add your action code here.
-        move(2);
-        turn(1);
+        if(Greenfoot.isKeyDown("d")){
+            move(2);
+        }
+        if(Greenfoot.isKeyDown("a")){
+            move(-2);
+        }
     }
 }
