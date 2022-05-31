ccd41762650f8b9dc8c3139058d3f4e09ad59f64
Stewart Chan <stew.chan@gmail.com>
2022-05-31 08:33:03 -0400

    scaled and flipped elephant

--- Elephant.java
+++ Elephant.java
@@ -2,21 +2,38 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Elephant extends Actor
 {
+    private boolean isFacingRight = true;
+    GreenfootImage rightImage;
+    GreenfootImage leftImage;
+    
     public Elephant()
     {
-        GreenfootImage image = new GreenfootImage("images/elephant_idle/idle0.png");
-        setImage(image);
+        rightImage = new GreenfootImage("images/elephant_idle/idle0.png");
+        rightImage.scale(100,100);
+        leftImage = new GreenfootImage("images/elephant_idle/idle0.png");
+        leftImage.scale(100,100);
+        leftImage.mirrorHorizontally();
+        setImage(rightImage);
     }
     
     public void act()
     {
         // Add your action code here.
         if(Greenfoot.isKeyDown("d")){
+            isFacingRight = true;
             move(3);
         }
         if(Greenfoot.isKeyDown("a")){
+            isFacingRight = false;
             move(-3);
         }
+        
+        if(isFacingRight){
+            setImage(rightImage);
+        } else {
+            setImage(leftImage);
+        }
+        
         
         // check if we eat an apple
         if(isTouching(Apple.class)){
