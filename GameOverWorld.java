9ded53ae0435d3adab446262a6578f200ceff074
Stewart Chan <stew.chan@gmail.com>
2022-06-06 09:17:15 -0400

    added label

--- GameOverWorld.java
+++ GameOverWorld.java
@@ -17,5 +17,7 @@ public class GameOverWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        Label label = new Label("Game Over", 100);
+        addObject(label, 300, 200);
     }
 }
