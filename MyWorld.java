1f62b1f49062e7dd55a2181fa1cf5d60cc4e3968
Stewart Chan <stew.chan@gmail.com>
2022-06-14 08:56:42 -0400

    removed rock

--- MyWorld.java
+++ MyWorld.java
@@ -13,32 +13,30 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1, false);
-        
+
         Elephant e = new Elephant();
         addObject(e, 300, 350);
-        
-        Rock rock = new Rock();
-        addObject(rock, 0, 0);
+
         
         scoreLabel = new Label(score, 80);
         addObject(scoreLabel, 50, 50);
-        
+
         spawnApple();
-        
+
     }
-    
+
     public void spawnApple(){
         Apple apple = new Apple();
         int x = Greenfoot.getRandomNumber(600);
         int y = 0;
         addObject(apple, x, y);
     }
-    
+
     public void increaseScore(){
         score++;
         scoreLabel.setValue(score);
     }
-    
+
     public void gameOver(){
         Label gameOverLabel = new Label("Game Over!", 80);
         addObject(gameOverLabel, 300, 200);
